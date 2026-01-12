3fb5d04dbbef741a52755c8b889b14222aa1ff6a
Wase <mefor2025@gmail.com>
2026-01-12 23:28:44 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,5 @@
-const API = "https://script.google.com/macros/s/AKfycbyj4yPuQyJkLjy8kpXRs1cZZ3rZmxHSKwKTG6KHzLQ2QtpcJWw8q_bMW5esfYvhVAWQ/exec";
+const API="https://script.google.com/macros/s/AKfycbyj4yPuQyJkLjy8kpXRs1cZZ3rZmxHSKwKTG6KHzLQ2QtpcJWw8q_bMW5esfYvhVAWQ/exec";
 
 function hideBroken(img){
-  img.onerror = () => img.style.display = "none";
-}
-
-async function loadProducts(target){
-  const res = await fetch(API + "?action=products");
-  const data = await res.json();
-
-  target.innerHTML = data.map(p => `
-    <a class="card" href="product.html?id=${p.id}">
-      <img src="${p.images[0] || ""}" onerror="hideBroken(this)">
-      <div class="info">
-        ${p.name}<br>
-        ₹${p.price}
-      </div>
-    </a>
-  `).join("");
+  img.onerror=()=>img.style.display="none";
 }
